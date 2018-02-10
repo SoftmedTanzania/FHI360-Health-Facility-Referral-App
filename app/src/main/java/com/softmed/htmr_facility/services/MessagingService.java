bbac445c41b060a9d0406f6148610927ffaddd82
issyzac <issyzac.iz@gmail.com>
2018-02-10 13:07:37 +0300

    changed reveiving implementations of push notification on development

--- app/src/main/java/com/softmed/htmr_facility/services/MessagingService.java
+++ app/src/main/java/com/softmed/htmr_facility/services/MessagingService.java
@@ -1,5 +1,8 @@
 package com.softmed.htmr_facility.services;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.content.LocalBroadcastManager;
@@ -11,6 +14,8 @@ import com.google.firebase.messaging.RemoteMessage;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import com.google.gson.Gson;
+import com.softmed.htmr_facility.R;
 import com.softmed.htmr_facility.activities.HomeActivity;
 import com.softmed.htmr_facility.base.AppDatabase;
 import com.softmed.htmr_facility.dom.objects.Patient;
@@ -42,16 +47,9 @@ public class MessagingService extends FirebaseMessagingService {
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated.
         Log.d(TAG, "From: " + remoteMessage.getFrom());
-        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
 
         if (remoteMessage == null)
             return;
-
-        // Check if message contains a notification payload.
-        if (remoteMessage.getNotification() != null) {
-            Log.e(TAG, "Notification Body: " + remoteMessage.getNotification().getBody());
-            handleNotification(remoteMessage.getNotification().getBody());
-        }
 
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
@@ -67,136 +65,91 @@ public class MessagingService extends FirebaseMessagingService {
 
     }
 
-    private void handleNotification(String message) {
-        if (!NotificationUtils.isAppIsInBackground(getApplicationContext())) {
-            /*
-            TODO
-            Broadcast is sent to all listening activities that a notification has been received
-            use this broadcast to listen and update the notifications list in main activity
+    private void triggerNotification(String msg)
+    {
+        Notification.Builder builder =
+                new Notification.Builder(this)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("Htmr Facility")
+                        .setAutoCancel(true)
+                        .setContentIntent(PendingIntent.getActivity(this, 0, new Intent(this, HomeActivity.class), 0))
+                        .setContentText(msg);
 
-            // app is in foreground, broadcast the push message
-            */
-            Intent pushNotification = new Intent(Config.PUSH_NOTIFICATION);
-            pushNotification.putExtra("message", message);
-            LocalBroadcastManager.getInstance(this).sendBroadcast(pushNotification);
+        Notification notification = builder.build();
+        NotificationManager notificationManager = (android.app.NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notification);
 
-            // play notification sound
-            //NotificationUtils notificationUtils = new NotificationUtils(getApplicationContext());
-            //notificationUtils.playNotificationSound();
-        }else{
-            // If the app is in background, firebase itself handles the notification
-        }
     }
 
     private void handleDataMessage(JSONObject json) {
-        Log.e(TAG, "push json: " + json.toString());
 
         Patient patient = new Patient();
         Referral referral = new Referral();
+        Gson gson = new Gson();
+
+        try {
+            JSONObject data = new JSONObject(json.toString());
+            String type = data.getString("type");
+            if (type.equals("PatientReferral")){
+                triggerNotification("Umepokea Rufaa Mpya");
+                patient = gson.fromJson(data.getJSONObject("patientsDTO").toString(), Patient.class);
+                database.patientModel().addPatient(patient);
+                Log.d("handleNotification", "added a patient");
+
+                JSONArray referralsDTOS = data.getJSONArray("patientReferralsList");
+                for (int i=0; i<referralsDTOS.length(); i++){
+                    referral = gson.fromJson(referralsDTOS.getJSONObject(i).toString(), Referral.class);
+                    database.referalModel().addReferal(referral);
+                    Log.d("handleNotification", "added Patient's Referral");
+                }
+            }else if (type.equals("PatientRegistration")){
+                triggerNotification("Umepokea Mteja Mpya");
+                JSONObject patientDTOS = new JSONObject(json.toString());
+                patient = gson.fromJson(patientDTOS.toString(), Patient.class);
+                database.patientModel().addPatient(patient);
+                Log.d("handleNotification", "added Registered Patient");
+
+            }else if (type.equals("ReferralFeedback")){
+                Log.d("handleNotification", "Received Feedback");
+                triggerNotification("Umepokea Mrejesho wa Rufaa");
+                JSONObject referralDTOS = new JSONObject(json.toString());
+                referral = gson.fromJson(referralDTOS.toString(), Referral.class);
+                database.referalModel().addReferal(referral);
+                Log.d("handleNotification", "added Referral Feedback");
+            }
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        Log.e(TAG, "push json: " + json.toString());
 
         try {
 
-            JSONObject data = new JSONObject(json.toString());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
-            JSONObject patientDTOS = data.getJSONObject("patientsDTO");
+        String msg = "Umepokea Data";
 
-            String patientFirstName = patientDTOS.getString("firstName");
-            String patientPhoneNumber = patientDTOS.getString("phoneNumber");
-            String patientGender = patientDTOS.getString("gender");
-            String patientSurname = patientDTOS.getString("surname");
-            String patientMiddleName = patientDTOS.getString("middleName");
-            int patientId = patientDTOS.getInt("patientId");
-            long patientDateOfBirth = patientDTOS.getLong("dateOfBirth");
-            long patientDateOfDeath = patientDTOS.getLong("dateOfDeath");
-            boolean patientHivStatus = patientDTOS.getBoolean("hivStatus");
+        if (!NotificationUtils.isAppIsInBackground(getApplicationContext())) {
+            // app is in foreground, broadcast the push message
+            Intent pushNotification = new Intent(Config.PUSH_NOTIFICATION);
+            pushNotification.putExtra("message", msg);
+            LocalBroadcastManager.getInstance(this).sendBroadcast(pushNotification);
 
-            patient.setPatientFirstName(patientFirstName);
-            patient.setPatientMiddleName(patientMiddleName);
-            patient.setPatientSurname(patientSurname);
-            patient.setPhone_number(patientPhoneNumber);
-            patient.setGender(patientGender);
-            patient.setPatientId(patientId+"");
-            patient.setDateOfBirth(patientDateOfBirth);
-            patient.setDateOfDeath(patientDateOfDeath);
-            patient.setHivStatus(patientHivStatus);
+            //play notification sound
+            //NotificationUtils notificationUtils = new NotificationUtils(getApplicationContext());
+            //notificationUtils.playNotificationSound();
+        } else {
 
-            Log.d("FinalTAg", "Patient : "+patient.getPatientId());
-            database.patientModel().addPatient(patient);
+            Intent resultIntent = new Intent(getApplicationContext(), HomeActivity.class);
+            resultIntent.putExtra("message", "");
 
-            JSONArray referralsDTOS = data.getJSONArray("referralsDTOS");
-            for (int i=0; i<referralsDTOS.length(); i++){
-                    JSONObject referralObject = referralsDTOS.getJSONObject(i);
+            showNotificationMessage(getApplicationContext(), "HTMR", msg, null, resultIntent);
 
-                    referral.setReferralReason(referralObject.getString("referralReason"));
-                    referral.setFacilityId(referralObject.getString("facilityId"));
-                    referral.setServiceProviderUIID(referralObject.getString("serviceProviderUIID"));
-                    referral.setServiceProviderGroup(referralObject.getString("serviceProviderGroup"));
-                    referral.setCommunityBasedHivService(referralObject.getString("communityBasedHivService"));
-                    referral.setVillageLeader(referralObject.getString("villageLeader"));
-                    referral.setCtcNumber(referralObject.getString("ctcNumber"));
-
-                    referral.setTestResults(referralObject.getBoolean("testResults"));
-
-
-                    /*referral.setHasSevereSweating(referralObject.getBoolean("hasSevereSweating"));
-                    referral.setHasFever(referralObject.getBoolean("hasFever"));
-                    referral.setHadWeightLoss(referralObject.getBoolean("hadWeightLoss"));
-                    referral.setHasBloodCough(referralObject.getBoolean("hasBloodCough"));
-                    referral.setHas2WeeksCough(referralObject.getBoolean("has2WeeksCough"));*/
-
-                    referral.setReferralStatus(referralObject.getInt("referralStatus"));
-                    referral.setPatient_id(referralObject.getInt("patientId")+"");
-                    try {
-                        referral.setFacilityId(referralObject.getInt("fromFacilityId")+"");
-                    }catch (Exception e){
-                        e.printStackTrace();
-                    }
-                    referral.setReferral_id(referralObject.getInt("referralId")+"");
-                    referral.setReferralSource(referralObject.getInt("referralSource"));
-                    referral.setServiceId(referralObject.getInt("serviceId"));
-                    referral.setReferralDate(referralObject.getLong("referralDate"));
-
-                    Log.d("FinalTAg", "Referral : "+referral.getReferralReason());
-
-                    database.referalModel().addReferal(referral);
-
-                }
-
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-
-            String msg = "Data Received";
-
-            if (!NotificationUtils.isAppIsInBackground(getApplicationContext())) {
-                // app is in foreground, broadcast the push message
-                Intent pushNotification = new Intent(Config.PUSH_NOTIFICATION);
-                pushNotification.putExtra("message", msg);
-                LocalBroadcastManager.getInstance(this).sendBroadcast(pushNotification);
-
-                // play notification sound
-                //NotificationUtils notificationUtils = new NotificationUtils(getApplicationContext());
-                //notificationUtils.playNotificationSound();
-            } else {
-                // app is in background, show the notification in notification tray
-                Intent resultIntent = new Intent(getApplicationContext(), HomeActivity.class);
-                resultIntent.putExtra("message", "");
-
-                showNotificationMessage(getApplicationContext(), "HTMR", msg, null, resultIntent);
-
-                // check for image attachment
-                /*if (TextUtils.isEmpty(imageUrl)) {
-                    showNotificationMessage(getApplicationContext(), title, message, timestamp, resultIntent);
-                } else {
-                    // image is present, show notification with image
-                    showNotificationMessageWithBigImage(getApplicationContext(), title, message, timestamp, resultIntent, imageUrl);
-                }*/
-
-                // play notification sound
-                //NotificationUtils notificationUtils = new NotificationUtils(getApplicationContext());
-                //notificationUtils.playNotificationSound();
-
-            }
+        }
 
     }
 
