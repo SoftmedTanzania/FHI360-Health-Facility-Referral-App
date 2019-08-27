582726ab7eb7350a784e96ee1641623cf89fcccb
cozej4 <ilakozejumanne@gmail.com>
2019-08-27 18:34:03 +0300

    Updated google-services.json

--- app/src/main/java/com/softmed/htmr_facility/utils/constants.java
+++ app/src/main/java/com/softmed/htmr_facility/utils/constants.java
@@ -7,7 +7,7 @@ package com.softmed.htmr_facility.utils;
 public class constants {
 
 //    public static String BASE_URL = "http://45.56.90.103:8080/opensrp/";
-    public static String BASE_URL = "http://139.162.184.148:8080/opensrp/";
+    public static String BASE_URL = "http://192.81.128.241:8080/opensrp/";
 
     //APP DATA NAMES
     public static final String SYNC_STATUS = "sync_status";
